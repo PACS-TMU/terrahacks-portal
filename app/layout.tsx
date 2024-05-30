c9f22a606c29fa1ec6eb3c7c3b6e55d9faa5bd60
William Khine <william@khine.dev>
2024-05-29 23:08:07 -0400

    change meta title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TerraHacks | Login or Sign Up",
+  description: "TerraHacks Hacker Portal",
 };
 
 export default function RootLayout({
