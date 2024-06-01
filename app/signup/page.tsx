fb47d8719d8cdd153f07a0232fba6d5695a09300
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-06-01 14:24:54 -0400

    fix: htmlfor parameters

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -61,26 +61,6 @@ export default function Signup({
         <div className="bg-gradient-to-b from-[#afd6e3] from-20% via-[#c3aa8e] via-50% to-[#432c2b] to-90% min-h-screen w-full flex items-center justify-center">
 
             <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md items-center justify-center gap-2">
-                {/* <Link
-                    href="/"
-                    className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-                    >
-                        <polyline points="15 18 9 12 15 6" />
-                    </svg>{" "}
-                    Back
-                </Link> */}
 
                 <img src="/images/Terrahacks_logo.png" alt="Terrahacks logo" className="w-screen mx-auto" />
 
@@ -88,29 +68,29 @@ export default function Signup({
                 <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
                     <h1 className="text-2xl text-bold pb-8 text-left  text-white">Application Portal</h1>
 
-                    <label className="text-md text-white" htmlFor="email">
+                    <label className="text-md text-white" htmlFor="firstname">
                         First Name
                     </label>
                     <input
-                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-6 placeholder-colour"
+                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-4 placeholder-colour"
                         name="firstname"
                         placeholder="Peter"
                         required
                     />
-                    <label className="text-md  text-white" htmlFor="email">
+                    <label className="text-md text-white" htmlFor="lastname">
                         Last Name
                     </label>
                     <input
-                        className="rounded-md px-4 py-2 bg-inherit border border-white  mb-6 placeholder-colour"
+                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-4 placeholder-colour"
                         name="lastname"
                         placeholder="Parker"
                         required
                     />
-                    <label className="text-md  text-white" htmlFor="email">
+                    <label className="text-md text-white" htmlFor="email">
                         Email
                     </label>
                     <input
-                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-6 placeholder-colour"
+                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-4 placeholder-colour"
                         name="email"
                         placeholder="you@terrahacks.ca"
                         required
@@ -119,7 +99,7 @@ export default function Signup({
                         Password
                     </label>
                     <input
-                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-6 placeholder-colour"
+                        className="rounded-md px-4 py-2 bg-inherit border border-white mb-4 placeholder-colour"
                         type="password"
                         name="password"
                         placeholder="super secret password"
