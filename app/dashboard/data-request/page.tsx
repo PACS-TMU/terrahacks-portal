c3ed703f68c1c70dc5c8903d22139b29a14b2d32
monakouzi <mo.nakouzi03@gmail.com>
2024-06-24 23:33:23 -0400

    adding data-request skeleton

---
+++ app/dashboard/data-request/page.tsx
@@ -0,0 +1,17 @@
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
+export default async function DataRequest() {
+    const supabase = createClient();
+
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (!user) {
+        return redirect('/login');
+    }
+
+    return (
+        <>
+        </>
+    );
+}
