b91c7fa0c6d873ebea27c522b3a4a20d451d1700
monakouzi <mo.nakouzi03@gmail.com>
2024-07-26 02:24:23 -0400

    adding ticket responsiveness and RSVP handling

--- app/dashboard/ticket/page.tsx
+++ app/dashboard/ticket/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import Ticket from "@/components/ticket";
+import Link from "next/link";
 
 export default async function Team() {
     const supabase = createClient();
@@ -11,6 +12,13 @@ export default async function Team() {
 
     if (!user) {
         return redirect("/login");
+    }
+
+    const { data: details, error } = await supabase.from("applications").select('rsvp').eq('account_id', user.id).single();
+
+    if (error) {
+        console.error(error);
+        return redirect("/dashboard?error=An error occurred while fetching your application details.");
     }
 
     return (
@@ -39,9 +47,30 @@ export default async function Team() {
                     </a> for the latest updates and information!
                 </p>
             </div>
-            <div>
-                <Ticket user_id={user.id} />
-            </div>
+            {details.rsvp === 'N/A' ? (
+                <div className="flex items-center justify-center w-11/12 md:w-3/4 lg:w-1/2 mx-auto my-[20vh] py-10 bg-highlight rounded-md shadow-sm">
+                    <p className="text-xl text-background font-bold font-sans">
+                        You have not RSVP'd yet. Please RSVP in the {" "}
+                        <Link
+                            href={'/dashboard'}
+                            rel="noopener noreferrer"
+                            className="text-gray-300 font-bold underline hover:text-gray-400 duration-300 ease-in-out">
+                            dashboard
+                        </Link> {" "}
+                        to get your ticket.
+                    </p>
+                </div>
+            ) : details.rsvp === 'Rescinded' ? (
+                <div className="flex items-center justify-center w-11/12 md:w-3/4 lg:w-1/2 mx-auto my-[20vh] py-10 bg-highlight rounded-md shadow-sm">
+                    <p className="text-xl text-background font-bold font-sans">
+                        You RSVP has been rescinded. Please contact us if you think this is a mistake.
+                    </p>
+                </div>
+            ) : (
+                <div>
+                    <Ticket user_id={user.id} />
+                </div>
+            )}
         </>
     );
 }
--- components/ticket.tsx
+++ components/ticket.tsx
@@ -1,28 +1,34 @@
 "use client";
-
 import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import QRCode from "react-qr-code";
-import Link from "next/link";
 
-export default function Ticket({user_id}: {user_id: string}) {
+export default function Ticket({ user_id }: { user_id: string }) {
+    const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+
     return (
-        <CardContainer className="inter-var">
-            <CardBody className="bg-[#306c84] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#306c84] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded text-white space-y-4">
+        <CardContainer className="inter-var w-11/12">
+            <CardBody className="bg-[#306c84] relative gr</CardContainer>oup/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#306c84] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded text-white space-y-4">
                 <CardItem
                     translateZ="50"
-                    className="px-6 pt-6 flex flex-row justify-between w-full"
+                    className="px-2 md:px-6 pt-6 flex flex-row justify-between w-full"
                 >
-                    <Image src={'/assets/th-text.png'} alt={'terrahacks-logo'} width={300} height={10} />
-                    <div className="flex flex-row space-x-4">
+                    <Image
+                        src={'/assets/th-text.png'}
+                        alt={'terrahacks-logo'}
+                        width={3000}
+                        height={400}
+                        className="w-[60%] max-w-full h-auto object-contain"
+                    />
+                    <div className="flex flex-row space-x-6">
                         <div className={'flex flex-col'}>
-                            <span className={'text-sm text-gray-400 text-right'}>DATE</span>
+                            <span className={'text-sm text-gray-200 text-right'}>DATE</span>
                             <span className={''}>Aug 2</span>
                         </div>
                         <div className={'flex flex-col'}>
-                            <span className={'text-sm text-gray-400 text-right'}>TIME</span>
-                            <span className={''}>XX:XX</span>
+                            <span className={'text-sm text-gray-200 text-right'}>TIME</span>
+                            <span className={''}>{currentTime}</span>
                         </div>
                     </div>
                 </CardItem>
@@ -35,22 +41,22 @@ export default function Ticket({user_id}: {user_id: string}) {
                         alt="thumbnail"
                     />
                 </CardItem>
-                <CardItem translateZ={50} className={'px-6'}>
-                    <div className={'text-sm text-gray-400'}>EVENT</div>
+                <CardItem translateZ={50} className={'px-3 md:px-6'}>
+                    <div className={'text-sm text-gray-200'}>EVENT</div>
                     <div>TerraHacks - Hackathon</div>
                 </CardItem>
-                <CardItem translateZ={50} className={'px-6 flex flex-row justify-between w-full'}>
+                <CardItem translateZ={50} className={'px-3 md:px-6 flex flex-row w-full justify-between text-sm md:text-sm'}>
                     <div>
-                        <div className={'text-sm text-gray-400'}>LOCATION</div>
-                        <div>Toronto Metropolitan University</div>
+                        <div className={'text-gray-200'}>LOCATION</div>
+                        <div>Toronto Metropolitan<br />University</div>
                     </div>
                     <div>
-                        <div className={'text-sm text-gray-400'}>CHECK-IN</div>
-                        <div>19:00</div>
+                        <div className={'text-gray-200'}>CHECK-IN</div>
+                        <div>18:00</div>
                     </div>
                     <div>
-                        <div className={'text-sm text-gray-400'}>ROOM</div>
-                        <div>DCC XXX</div>
+                        <div className={'text-gray-200'}>ROOM</div>
+                        <div>DCC 103</div>
                     </div>
                 </CardItem>
                 <CardItem translateZ={100} className={'mx-auto pb-6'}>
