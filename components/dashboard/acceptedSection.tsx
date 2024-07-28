936e3c45c07aabb6f1676ad4d505e8476c649388
Mo Nakouzi <mo.nakouzi03@gmail.com>
2024-07-28 14:12:20 -0400

    Disabling RSVP button when max RSVPs is reached (#51)
    The RSVP button will now be disables when we reach RSVP capacity of 250.
    
    ---------
    
    Co-authored-by: Saadia Shahid <saadia.shahid@torontomu.ca>

--- components/dashboard/acceptedSection.tsx
+++ components/dashboard/acceptedSection.tsx
@@ -1,20 +1,44 @@
 import { SubmitButton } from "../forms/submit-button";
 import submitRSVP from "@/server/submitRSVP";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function AcceptedSection() {
+export default async function AcceptedSection() {
+    const maxRSVPs = 250;
+
+    const supabase = createClient();
+    const { count: yesCount, error } = await supabase.from("applications").select("rsvp", { count: 'exact' })
+        .eq("rsvp", "Yes");
+
+    if (error) {
+        console.error(error);
+        return redirect("/dashboard?message=Error - Error retrieving RSVP data, please contact support if issue persists.");
+    }
+    const rsvpDisabled = (yesCount !== null && yesCount >= maxRSVPs);
+
     return (
         <div className="bg-highlight text-background rounded-md my-2 md:mb-0 md:mt-4 font-sans p-2 flex flex-col justify-center items-center w-[90%] md:w-2/3 xl:w-1/2 mx-auto">
-            <p className="md:text-xl whitespace-pre-line text-center my-2">Congratulations, you've been accepted!</p>
-            <p className="md:text-xl whitespace-pre-line text-center my-2">RSVP below as soon as possible to guarantee yourself a spot. We look forward to seeing you in August!</p>
+
+            <p className="md:text-xl whitespace-pre-line text-center my-2">
+                Congratulations, you've been accepted!
+            </p>
+            <p className="md:text-xl whitespace-pre-line text-center my-2">
+                {rsvpDisabled ?
+                    "Unfortunately, the RSVP period has ended. Please contact us if you have any questions." :
+                    "RSVP below as soon as possible to guarantee yourself a spot. \nWe look forward to seeing you in August!"
+                }
+            </p>
+
             <form>
                 <SubmitButton
+                    disabled={rsvpDisabled}
                     pendingText="RSVPing..."
                     aria-label="RSVP to TerraHacks 2024"
                     type="submit"
-                    className="font-semibold bg-background p-3 rounded-md text-foreground text-lg md:text-xl duration-300 ease-in-out hover:bg-gray-200 my-4"
+                    className={`font-semibold  p-3 rounded-md text-foreground text-lg md:text-xl duration-300 ease-in-out ${rsvpDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-background hover:bg-gray-200'}  my-4`}
                     formAction={submitRSVP}
                 >
-                    RSVP Now!
+                    {rsvpDisabled ? "RSVP period has ended :(" : "RSVP Now!"}
                 </SubmitButton>
             </form>
         </div>
