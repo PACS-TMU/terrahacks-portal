f4ec37403a1ea8a1c06f7df7c9cd28684c48a59f
Mo Nakouzi <mo.nakouzi03@gmail.com>
2025-06-16 12:03:08 -0400

    tailwind update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+const tailwindcss = require('@tailwindcss/postcss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(),
+    require('autoprefixer'),
+  ],
 };
