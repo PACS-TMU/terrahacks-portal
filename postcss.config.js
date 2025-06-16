ba3e39be50bae99ca22f156c22283e57086e73ca
Mo Nakouzi <mo.nakouzi03@gmail.com>
2025-06-16 12:42:48 -0400

    upgrading to tailwind v4 and other dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-const tailwindcss = require('@tailwindcss/postcss');
-
 module.exports = {
-  plugins: [
-    tailwindcss(),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
